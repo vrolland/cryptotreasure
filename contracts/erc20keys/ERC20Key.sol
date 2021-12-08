cffddbf75269621ff42842a2b999a9c10ad86658
vrolland <vrolland@protonmail.com>
2021-12-08 19:26:08 +0100

    fix key approval and naming

--- contracts/erc20keys/ERC20Key.sol
+++ contracts/erc20keys/ERC20Key.sol
@@ -9,20 +9,19 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
  * @notice mintable only by admins, no approval needed for a contract (cryptotreasure)
  */
 contract ERC20Key is ERC20, AccessControl {
-    uint256 MAX_UINT;
-
     address public cryptotreasure;
 
     /**
      * @dev Constructor
      * @param cryptotreasure_ cryptotreasure address
      */
-    constructor(address cryptotreasure_) ERC20("CryptoKey", "CKY") {
+    constructor(
+        string memory name,
+        string memory symbol,
+        address cryptotreasure_
+    ) ERC20(name, symbol) {
         cryptotreasure = cryptotreasure_;
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        unchecked {
-            MAX_UINT = 2**256 - 1;
-        }
     }
 
     function mint(address account, uint256 amount)
@@ -30,7 +29,6 @@ contract ERC20Key is ERC20, AccessControl {
         onlyRole(DEFAULT_ADMIN_ROLE)
     {
         _mint(account, amount);
-        _approve(account, cryptotreasure, MAX_UINT);
     }
 
     function mintMultiple(
@@ -39,7 +37,33 @@ contract ERC20Key is ERC20, AccessControl {
     ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         for (uint256 j = 0; j < accounts.length; j++) {
             _mint(accounts[j], amounts[j]);
-            _approve(accounts[j], cryptotreasure, MAX_UINT);
+        }
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public override returns (bool) {
+        if (_msgSender() == cryptotreasure) {
+            _transfer(sender, recipient, amount);
+        } else {
+            super.transferFrom(sender, recipient, amount);
+        }
+
+        return true;
+    }
+
+    function allowance(address owner, address spender)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        if (_msgSender() == cryptotreasure) {
+            return type(uint256).max;
+        } else {
+            return super.allowance(owner, spender);
         }
     }
 
